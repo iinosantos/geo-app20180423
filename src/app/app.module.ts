00eaceafc50927d32aa16746c4b51b192695fc66
isabella <midinosantos@gmail.com>
2018-04-23 20:31:35 +0800

    Rearranged modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AgmCoreModule } from '@agm/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatMomentDateModule} from '@angular/material-moment-adapter';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { GoogleMapsAPIWrapper } from '@agm/core';
+import { MatButtonModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatMomentDateModule} from '@angular/material-moment-adapter';
 
 import { AppComponent } from './app.component';
 
@@ -18,17 +19,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyAAfBkERU_PD51MZu7HTD2GCyUj3-i3ft4'
     }),
-    MatDatepickerModule,
-    MatMomentDateModule,
-    MatInputModule,
-    MatCardModule,
     BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
     MatButtonModule,
-    FormsModule
+    MatCardModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatMomentDateModule
   ],
   providers: [
     GoogleMapsAPIWrapper
